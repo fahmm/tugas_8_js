163f15061040c9d4cc8cfcb0c4da0144ca8a25d3
Fahmi <pammimajid@gmail.com>
2020-03-15 17:48:01 +0800

    ok

---
+++ tugas_8_js.js
@@ -0,0 +1,17 @@
+function panggilgundam(){
+	var gundam = {
+		type : 'gundam barbatos',
+		jenis: 'robot',
+		warna: 'putih'
+	}
+
+	gundam.warna = 'hitam';
+
+	gundam.senjata= 'pedang';
+
+	console.log(gundam)
+	console.log(gundam.warna)
+
+}
+
+panggilgundam()
